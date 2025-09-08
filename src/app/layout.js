a93fe754182d07bbcc2ccdf66c0d3294442feddd
Armando Rivas <armandorivasv.dev@gmail.com>
2025-09-08 17:57:14 -0300

    ready deploy construction page to vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,18 @@
 import LayoutProvider from '@/layouts/LayoutProvider';
 
 export const metadata = {
+  metadataBase: 'https://www.envasadosh2o.com',
   title: 'Envasados H2O',
-  description: 'Envasados H2O',
+
+  description: 'Agua Mineral Canaima, Sin calorias, Libre de sodio, 100% natural.',
+  openGraph: {
+    title: 'Envasados H2O',
+    description: 'Agua Mineral Canaima, Sin calorias, Libre de sodio, 100% natural.',
+    url: 'https://www.envasadosh2o.com',
+    siteName: 'envasadosh2o',
+    locale: 'es_VE',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
